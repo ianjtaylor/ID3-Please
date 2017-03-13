db71f19bce382ee95786bedda5734470cd59d14f
ianjtaylor <ianjtaylor1@gmail.com>
2017-03-12 20:01:31 -0400

    Initial Commit

---
+++ iTunes Matcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iTunes Matcher
+//
+//  Created by Ian Taylor on 3/12/17.
+//  Copyright © 2017 Ian Taylor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
